5f8d29de2ca0bc3584a99d1b34d676f075c77df7
aleksejbelkin1017 <aleksejbelkin1017@gmail.com>
2025-03-13 15:19:59 +0300

    add func get_sp500_data in module views.py

--- src/views.py
+++ src/views.py
@@ -1,6 +1,9 @@
 from typing import Optional
 
+import os
 import pandas as pd
+import requests
+import json
 
 
 def read_excel_transactions(file_path: str) -> Optional[list[dict]]:
@@ -13,6 +16,39 @@ def read_excel_transactions(file_path: str) -> Optional[list[dict]]:
         return None
 
 
-if __name__ == "__main__":
-    file = 'data/operations.xlsx'
-    print(read_excel_transactions(file))
+def get_sp500_data():
+    """ Функция получает данные об S&P 500 через API сервис Alpha Vantage """
+    try:
+        api_key = os.getenv('API_KEY_alphavantage')
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&apikey={api_key}'
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print("Ошибка HTTP:", response.text)
+            return
+
+        data = response.json()
+
+        if not data:
+            print("Получен пустой ответ")
+            return
+
+        # Записываем данные в файл
+        with open('data/sp500_data.json', 'w') as json_file:
+            json.dump(data, json_file, indent=2)
+
+        print("Данные успешно сохранены в файл sp500_data.json")
+
+    except requests.RequestException as e:
+        print(f"Ошибка при запросе: {e}")
+    except ValueError as e:
+        print(f"Ошибка при парсинге JSON: {e}")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+# if __name__ == "__main__":
+#    get_sp500_data()
+#    file = 'data/operations.xlsx'
+#    print(read_excel_transactions(file))
